fd98846ba3738ea4b61167c7efcd4b15ea4fd886
Linda-sunshine <gonglin1988@gmail.com>
2014-12-14 19:50:53 -0500

    fix outofbound

--- src/Analyzer/FeatureSelector.java
+++ src/Analyzer/FeatureSelector.java
@@ -50,8 +50,8 @@ public class FeatureSelector {
 		Collections.sort(sortedDF);//sort the DF
 		for(int DF : sortedDF) sortedFeatures.addAll(featureDF.get(DF));//sort the features according to sorted DF.
 			
-		//Start fetching particular features. How do we define the criterion?
-		int totalSize = featureStat.size();
+		//Start fetching particular features.
+		int totalSize = sortedFeatures.size();
 		int start = (int) (totalSize * this.m_startProb);
 		int end = (int) (totalSize * this.m_endProb);
 		ArrayList<String> selectedFeatures = new ArrayList<String>(sortedFeatures.subList(start, end));
@@ -80,13 +80,10 @@ public class FeatureSelector {
 				PrCiSum -= PrCi[i] * Math.log(PrCi[i]);
 			}
 		}
-		int featureSize2 = 0; // Use this to filter features' DF < threshold.
 		for(String f: featureStat.keySet()){
-			
 			//Filter the features which have smaller DFs.
 			int sumDF = Utils.sumOfArray(featureStat.get(f).getDF());
 			if (sumDF > threshold){
-				featureSize2++;
 				_stat temp = featureStat.get(f);
 				Prt = Utils.sumOfArray(temp.getDF()) / classMemberSum;
 				PrtNot = 1 - Prt;
@@ -117,57 +114,60 @@ public class FeatureSelector {
 		Collections.sort(sortedIG);//sort the DF
 		for(double IG : sortedIG) sortedFeatures.addAll(featureIG.get(IG));//sort the features according to sorted DF.
 			
-		int start = (int) (featureSize2 * this.m_startProb);
-		int end = (int) (featureSize2 * this.m_endProb);
+		int totalSize = sortedFeatures.size();
+		int start = (int) (totalSize * this.m_startProb);
+		int end = (int) (totalSize * this.m_endProb);
 		ArrayList<String> selectedFeatures = new ArrayList<String>(sortedFeatures.subList(start, end));
 		return selectedFeatures;
 	} 
 		
 	//Feature Selection -- MI.
 	public ArrayList<String> MI(HashMap<String, _stat> featureStat, int[] classMemberNo, int threshold){
-			HashMap<Double, ArrayList<String>> featureMI = new HashMap<Double, ArrayList<String>>();
-			ArrayList<String> sortedFeatures = new ArrayList<String>();
-			Collection<Double> unsortedMI = new HashSet<Double>();
-			
-			double[] PrCi = new double [classMemberNo.length]; 
-			double[] ItCi = new double [classMemberNo.length];
-			double N = Utils.sumOfArray(classMemberNo);
-			double Iavg = 0;
-			
-			for(int i = 0; i < classMemberNo.length; i++) PrCi[i] = classMemberNo[i] / N;
-			for(String f: featureStat.keySet()){
-				//Filter the features which have smaller DFs.
-				int sumDF = Utils.sumOfArray(featureStat.get(f).getDF());
-				if (sumDF > threshold){
-					Iavg = 0;
-					for(int i = 0; i < classMemberNo.length; i++){
-						_stat temp = featureStat.get(f);
-						double A = temp.getDF()[i];
-						ItCi[i] = Math.log(A * N / classMemberNo[i] * Utils.sumOfArray(temp.getDF()));
-						Iavg += ItCi[i] * PrCi[i];				
-					}
-					if(featureMI.containsKey(Iavg)){
-						featureMI.get(Iavg).add(f);
-					}else{
-						ArrayList<String> tempArray = new ArrayList<String>();
-						tempArray.add(f);
-						featureMI.put(Iavg, tempArray);
-					}
-				}else System.out.println("No features found as for threshold " + threshold);
+		HashMap<Double, ArrayList<String>> featureMI = new HashMap<Double, ArrayList<String>>();
+		ArrayList<String> sortedFeatures = new ArrayList<String>();
+		Collection<Double> unsortedMI = new HashSet<Double>();
+
+		double[] PrCi = new double[classMemberNo.length];
+		double[] ItCi = new double[classMemberNo.length];
+		double N = Utils.sumOfArray(classMemberNo);
+		double Iavg = 0;
+
+		for (int i = 0; i < classMemberNo.length; i++)
+			PrCi[i] = classMemberNo[i] / N;
+		for (String f : featureStat.keySet()) {
+			// Filter the features which have smaller DFs.
+			int sumDF = Utils.sumOfArray(featureStat.get(f).getDF());
+			if (sumDF > threshold) {
+				Iavg = 0;
+				for (int i = 0; i < classMemberNo.length; i++) {
+					_stat temp = featureStat.get(f);
+					double A = temp.getDF()[i];
+					ItCi[i] = Math.log(A * N / classMemberNo[i]
+							* Utils.sumOfArray(temp.getDF()));
+					Iavg += ItCi[i] * PrCi[i];
+				}
+				if (featureMI.containsKey(Iavg)) {
+					featureMI.get(Iavg).add(f);
+				} else {
+					ArrayList<String> tempArray = new ArrayList<String>();
+					tempArray.add(f);
+					featureMI.put(Iavg, tempArray);
+				}
 			}
-				
-			unsortedMI = featureMI.keySet();
-			ArrayList<Double> sortedMI = new ArrayList<Double>(unsortedMI);     
-			Collections.sort(sortedMI);//sort the DF
-			for(double MI : sortedMI) sortedFeatures.addAll(featureMI.get(MI));//sort the features according to sorted MI.
-			
-			//Start fetching particular features. How do we define the criterion?
-			int totalSize = featureStat.size();
-			int start = (int) (totalSize * this.m_startProb);
-			int end = (int) (totalSize * this.m_endProb);
-			ArrayList<String> selectedFeatures = new ArrayList<String>(sortedFeatures.subList(start, end));
-			return selectedFeatures;
 		}
+
+		unsortedMI = featureMI.keySet();
+		ArrayList<Double> sortedMI = new ArrayList<Double>(unsortedMI);
+		Collections.sort(sortedMI);// sort the DF
+		for (double MI : sortedMI) sortedFeatures.addAll(featureMI.get(MI));// sort the features according to sorted MI.
+
+		// Start fetching particular features. How do we define the criterion?
+		int totalSize = sortedFeatures.size();
+		int start = (int) (totalSize * this.m_startProb);
+		int end = (int) (totalSize * this.m_endProb);
+		ArrayList<String> selectedFeatures = new ArrayList<String>(sortedFeatures.subList(start, end));
+		return selectedFeatures;
+	}
 		
 	//Feature Selection -- CHI.
 	public ArrayList<String> CHI(HashMap<String, _stat> featureStat, int[] classMemberNo, int threshold){
@@ -204,15 +204,15 @@ public class FeatureSelector {
 					tempArray.add(f);
 					featureCHI.put(X2avg, tempArray);
 				}
-			} else System.out.println("No features found as for threshold " + threshold);
+			}
 		}
 		unsortedCHI = featureCHI.keySet();
 		ArrayList<Double> sortedCHI = new ArrayList<Double>(unsortedCHI);     
-		Collections.sort(sortedCHI);//sort the DF
-		for(double CHI : sortedCHI) sortedFeatures.addAll(featureCHI.get(CHI));//sort the features according to sorted MI.
+		Collections.sort(sortedCHI);//sort the CHI
+		for(double CHI : sortedCHI) sortedFeatures.addAll(featureCHI.get(CHI));//sort the features according to sorted CHI.
 			
 		//Start fetching particular features. How do we define the criterion?
-		int totalSize = featureStat.size();
+		int totalSize = sortedFeatures.size();
 		int start = (int) (totalSize * this.m_startProb);
 		int end = (int) (totalSize * this.m_endProb);
 		ArrayList<String> selectedFeatures = new ArrayList<String>(sortedFeatures.subList(start, end));
