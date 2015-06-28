17592e43df0f80accf65d22c190065467b6c7e74
wang296 <wang.hongn@gmail.com>
2015-06-28 10:55:21 -0400

    local change

--- src/mains/TransductiveMain.java
+++ src/mains/TransductiveMain.java
@@ -30,7 +30,7 @@ public class TransductiveMain {
 		int number_of_iteration = 100;
 		
 		/*****The parameters used in loading files.*****/
-		String folder = "./data/amazon/tablet/topicmodel";
+		String folder = "./data/amazon/tablet/small";
 		String suffix = ".json";
 		String tokenModel = "./data/Model/en-token.bin"; //Token model.
 		String stnModel = null;
