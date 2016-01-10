c60601e3acd882974b37bf19894b2a21a90fb094
jetcai1900 <jetcai1900@gmail.com>
2016-01-09 23:13:32 -0500

    add some attributes and functions to these two structures

--- src/structures/_Stn.java
+++ src/structures/_Stn.java
@@ -10,6 +10,12 @@ import java.util.Set;
  * Sentence structure for text documents
  */
 public class _Stn {
+	//added by Renqin
+	public int[] m_wordPositionInDoc;
+	public double m_stnLength;
+	public int[] m_words;
+	public double[] m_topics;
+
 	_SparseFeature[] m_x_sparse; // bag of words for a sentence
 	
 	//structure for HTMMM
@@ -34,6 +40,14 @@ public class _Stn {
 	int m_sentimentLabel = -1;
 	int m_predictedSentimentLabel = -1;
 	
+	//added by Renqin
+	public _Stn(int stnLength) {
+		m_stnLength = stnLength;
+		m_wordPositionInDoc = new int[stnLength];
+		m_rawTokens = new String[stnLength];
+		m_words = new int[stnLength]; // record index;
+	}
+
 	public _Stn(_SparseFeature[] x, String[] rawTokens, String[] posTags, String rawSource) {
 		m_x_sparse = x;
 		m_rawTokens = rawTokens;
@@ -53,6 +67,22 @@ public class _Stn {
 		
 		m_transitFv = new double[_Doc.stn_fv_size];
 		m_sentiTransitFv = new double[_Doc.stn_senti_fv_size];
+	}
+
+	// added by Renqin
+	public void setTopicsVct(int k) {
+		m_topics = new double[k];
+	}
+
+	//added by Renqin
+	public void setWordInStn(ArrayList<Integer> positionInStn,
+			ArrayList<String> rawTokens, ArrayList<Integer> words) {
+		for (int n = 0; n < positionInStn.size(); n++) {
+			m_wordPositionInDoc[n] = positionInStn.get(n);
+			m_rawTokens[n] = rawTokens.get(n);
+			m_words[n] = words.get(n);
+		}
+
 	}
 
 	public _SparseFeature[] getFv() {
