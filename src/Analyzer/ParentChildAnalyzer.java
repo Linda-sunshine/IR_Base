cacbf367aae7e9524f21ff847a819b2c18a3035d
jetcai1900 <jetcai1900@gmail.com>
2016-02-01 22:19:51 -0500

    fix a bug
    add arrayList

--- src/Analyzer/ParentChildAnalyzer.java
+++ src/Analyzer/ParentChildAnalyzer.java
@@ -3,6 +3,7 @@ package Analyzer;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import json.JSONArray;
