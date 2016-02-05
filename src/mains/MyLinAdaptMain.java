53ae9df2f91510ff09bcd15a6fccdf5bfab4b90c
Linda-sunshine <gonglin1988@gmail.com>
2016-02-05 12:09:54 -0500

    add MyLinAdaptMain.java

---
+++ src/mains/MyLinAdaptMain.java
@@ -0,0 +1,108 @@
+package mains;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import opennlp.tools.util.InvalidFormatException;
+import structures._Doc;
+import structures._Review;
+import structures._User;
+import Analyzer.MultiThreadedUserAnalyzer;
+import Classifier.semisupervised.CoLinAdapt.LinAdapt;
+import Classifier.supervised.modelAdaptation.MultiTaskSVM;
+import Classifier.supervised.modelAdaptation.CoLinAdapt.CoLinAdapt;
+import Classifier.supervised.modelAdaptation.CoLinAdapt.asyncCoLinAdapt;
+
+public class MyLinAdaptMain {
+	
+	//In the main function, we want to input the data and do adaptation 
+	public static void main(String[] args) throws InvalidFormatException, FileNotFoundException, IOException{
+
+		int classNumber = 2;
+		int Ngram = 2; // The default value is unigram.
+		int lengthThreshold = 5; // Document length threshold
+		double trainRatio = 0, adaptRatio = 1;
+		int topKNeighbors = 20;
+		int displayLv = 0;
+		int numberOfCores = Runtime.getRuntime().availableProcessors();
+//		double eta1 = 0.5, eta2 = 0.5, eta3 = 0.6, eta4 = 0.01, neighborsHistoryWeight = 0.5;
+		double eta1 = 1.3087, eta2 = 0.0251, eta3 = 1.7739, eta4 = 0.4859, neighborsHistoryWeight = 0.5;
+		boolean enforceAdapt = false;
+
+		String dataset = "Amazon"; // "Amazon", "Yelp"
+		String tokenModel = "./data/Model/en-token.bin"; // Token model.
+		String providedCV = String.format("./data/CoLinAdapt/%s/SelectedVocab.csv", dataset); // CV.
+		String userFolder = String.format("./data/CoLinAdapt/%s/Users", dataset);
+		String featureGroupFile = String.format("./data/CoLinAdapt/%s/CrossGroups.txt", dataset);
+		String globalModel = String.format("./data/CoLinAdapt/%s/GlobalWeights.txt", dataset);
+			
+//		UserAnalyzer analyzer = new UserAnalyzer(tokenModel, classNumber, providedCV, Ngram, lengthThreshold);
+		MultiThreadedUserAnalyzer analyzer = new MultiThreadedUserAnalyzer(tokenModel, classNumber, providedCV, Ngram, lengthThreshold, numberOfCores);
+		analyzer.config(trainRatio, adaptRatio, enforceAdapt);
+		analyzer.loadUserDir(userFolder); // load user and reviews
+		analyzer.setFeatureValues("TFIDF-sublinear", 0);
+		HashMap<String, Integer> featureMap = analyzer.getFeatureMap();
+		
+		// Load svd of each user.
+//		String svdFile = "./data/CoLinAdapt/Amazon/Amazon_SVD.mm";
+//		analyzer.loadSVDFile(svdFile);
+		
+//		 // Create an instances of LinAdapt model.
+//		 LinAdapt adaptation = new LinAdapt(classNumber, analyzer.getFeatureSize(), featureMap, globalModel,featureGroupFile);
+
+//		 // Create an instances of asyncLinAdapt model.
+//		 asyncLinAdapt adaptation = new asyncLinAdapt(classNumber,
+//		 analyzer.getFeatureSize(), featureMap, globalModel,
+//		 featureGroupFile);
+		
+		// Create an instances of CoLinAdapt model.
+//		CoLinAdapt adaptation = new CoLinAdapt(classNumber, analyzer.getFeatureSize(), featureMap, topKNeighbors, globalModel, featureGroupFile);
+		
+//		// Create an instance of CoLinAdaptWithNeighborhoodLearning model.
+//		int fDim = 3; // xij contains <bias, bow, svd_sim>
+//		CoLinAdaptWithNeighborhoodLearning adaptation = new CoLinAdaptWithNeighborhoodLearning(classNumber, analyzer.getFeatureSize(), featureMap, topKNeighbors, globalModel, featureGroupFile, fDim);
+		
+//		// Create an instances of zero-order asyncCoLinAdapt model.
+		asyncCoLinAdapt adaptation = new asyncCoLinAdapt(classNumber, analyzer.getFeatureSize(), featureMap, topKNeighbors, globalModel, featureGroupFile);
+
+		//Create an instances of first-order asyncCoLinAdapt model.
+//		asyncCoLinAdaptFirstOrder adaptation = new asyncCoLinAdaptFirstOrder(classNumber, analyzer.getFeatureSize(), featureMap, topKNeighbors, globalModel, featureGroupFile, neighborsHistoryWeight);
+		
+		int[] count = new int[2];
+		int total = 0;
+
+		for(_User u: analyzer.getUsers()){
+			total += u.getReviewSize();
+			for(_Review r: u.getReviews())
+				if(r.getYLabel() == 0)
+					count[0]++;
+				else count[1]++;
+		}
+//		for(_Doc r: analyzer.getCorpus().getCollection()){
+//			if(r.getYLabel() == 0)
+//				count[0]++;
+//			else
+//				count[1]++;
+//		}
+		System.out.format("neg: %d, pos: %d, total: %d", count[0], count[1], total);
+		
+		
+//		adaptation.loadUsers(analyzer.getUsers());
+//		adaptation.setDisplayLv(displayLv);
+//		// adaptation.setTestMode(TestMode.TM_batch);
+//		adaptation.setR1TradeOffs(eta1, eta2);
+//		adaptation.setR2TradeOffs(eta3, eta4);
+//
+//		adaptation.train();
+//		adaptation.test();
+//		adaptation.saveModel("data/results/colinadapt");
+
+		//Create the instance of MT-SVM
+//		MultiTaskSVM mtsvm = new MultiTaskSVM(classNumber, analyzer.getFeatureSize());
+//		mtsvm.loadUsers(analyzer.getUsers());
+//		mtsvm.setBias(true);
+//		mtsvm.train();
+//		mtsvm.test();
+//		mtsvm.saveModel("data/results/MTSVM");
+	}
+}
