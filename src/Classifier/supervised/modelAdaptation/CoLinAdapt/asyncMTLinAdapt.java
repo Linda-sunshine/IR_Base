595122d2410266375f0ad609b898822bf22f9576
Linda-sunshine <gonglin1988@gmail.com>
2016-04-13 15:57:04 -0400

    update the online training of MTLinAdapt

--- src/Classifier/supervised/modelAdaptation/CoLinAdapt/asyncMTLinAdapt.java
+++ src/Classifier/supervised/modelAdaptation/CoLinAdapt/asyncMTLinAdapt.java
@@ -1,10 +1,10 @@
 package Classifier.supervised.modelAdaptation.CoLinAdapt;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
-
 import Classifier.supervised.modelAdaptation._AdaptStruct;
 import Classifier.supervised.modelAdaptation.RegLR.asyncRegLR;
 import structures._PerformanceStat.TestMode;
@@ -12,10 +12,17 @@ import structures._Review;
 import structures._Review.rType;
 import structures._UserReviewPair;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import utils.Utils;
+
 public class asyncMTLinAdapt extends MTLinAdapt {
 
-	double m_initStepSize = 0.25;
+	double m_initStepSize = 0.05;
 	boolean m_trainByUser = false; // by default we will perform online training by user; otherwise we will do it by review timestamp 
+	int m_rptTime = 3, m_count = 0; // How many times the reviews will be used to update gradients.
 	
 	public asyncMTLinAdapt(int classNo, int featureSize,
 			HashMap<String, Integer> featureMap, int topK, String globalModel,
@@ -29,12 +36,18 @@ public class asyncMTLinAdapt extends MTLinAdapt {
 //		m_testmode = TestMode.TM_batch;
 	}
 	
-	@Override
-	public String toString() {
-		return String.format("asyncMTLinAdapt[dim:%d,SupDim:%d, eta1:%.3f,eta2:%.3f, lambda1:%.3f,lambda2:%.3f]", m_dim, m_dimSup, m_eta1, m_eta2, m_lambda1, m_lambda2);
+	public void setRPTTime(int t){
+		m_rptTime = t;
+	}
+	public void setTrainByUser(boolean b){
+		m_trainByUser = b;
 	}
 	
 	@Override
+	public String toString() {
+		return String.format("asyncMTLinAdapt[dim:%d,SupDim:%d, eta1:%.3f,eta2:%.3f, lambda1:%.3f, lambda2:%.3f]", m_dim, m_dimSup, m_eta1, m_eta2, m_lambda1, m_lambda2);
+	}
+
 	protected void calculateGradients(_AdaptStruct u){
 		gradientByFunc(u);
 		gradientByR1(u);
@@ -44,10 +57,10 @@ public class asyncMTLinAdapt extends MTLinAdapt {
 	@Override
 	protected void init(){
 		super.init();
-		
-		// this is also incorrect, since we should normalized it by total number of adaptation reviews
-		m_lambda1 /= m_userSize;
-		m_lambda2 /= m_userSize;
+	 		
+	 	// this is also incorrect, since we should normalized it by total number of adaptation reviews
+	 	m_lambda1 /= m_userSize;
+	 	m_lambda2 /= m_userSize;
 	}
 	
 	//this is online training in each individual user
@@ -63,7 +76,7 @@ public class asyncMTLinAdapt extends MTLinAdapt {
 		
 		setPersonalizedModel();
 		return 0;//we do not evaluate function value
-	}	
+	}
 	
 	void trainByReview() {
 		LinkedList<_UserReviewPair> reviewlist = new LinkedList<_UserReviewPair>();
@@ -149,16 +162,21 @@ public class asyncMTLinAdapt extends MTLinAdapt {
 				System.out.println();
 		}
 	}
+
 	
 	@Override
 	protected int getAdaptationSize(_AdaptStruct user) {
 		return user.getAdaptationCacheSize();
 	}
 	
+	// 
+	public void resetRPTTime(){
+		m_count = m_rptTime;
+	}
 	@Override
 	protected void gradientByFunc(_AdaptStruct user) {		
 		//Update gradients one review by one review.
-		for(_Review review:user.nextAdaptationIns())
+		for(_Review review: user.nextAdaptationIns())
 			gradientByFunc(user, review, 1.0);//equal weight for the user's own adaptation data
 	}
 	
@@ -171,23 +189,73 @@ public class asyncMTLinAdapt extends MTLinAdapt {
 		Arrays.fill(m_g, 0);
 		calculateGradients(user);
 		
-		//update the individual user
-		for (int k = 0; k < m_dim; k++) {
-			a = user.getScaling(k) - stepSize * m_g[offset + k];
-			user.setScaling(k, a);
+		resetRPTTime();
+		while(m_count-- > 0){
+			//update the individual user
+			for (int k = 0; k < m_dim; k++) {
+				a = user.getScaling(k) - stepSize * m_g[offset + k];
+				user.setScaling(k, a);
 
-			b = user.getShifting(k) - stepSize * m_g[offset + k + m_dim];
-			user.setShifting(k, b);
-		}
+				b = user.getShifting(k) - stepSize * m_g[offset + k + m_dim];
+				user.setShifting(k, b);
+			}
 		
-		//update the super user
-		stepSize /= 3;
-		for(int k=0; k<m_dimSup; k++) {
-			m_A[supOffset+k] -= stepSize * m_g[supOffset + k];
-			m_A[supOffset+k+m_dimSup] -= stepSize * m_g[supOffset + k + m_dimSup];
-		}
+			//update the super user
+			stepSize /= 3;
+			for(int k=0; k<m_dimSup; k++) {
+				m_A[supOffset+k] -= stepSize * m_g[supOffset + k];
+				m_A[supOffset+k+m_dimSup] -= stepSize * m_g[supOffset + k + m_dimSup];
+			}
 		
-		//update the record of updating history
-		user.incUpdatedCount(inc);
+			//update the record of updating history
+			if(m_count == 0)
+				user.incUpdatedCount(inc);
+		}
+	}
+	
+	public void loadGlobal(String filename){
+		if (filename==null)
+			return;	
+		try{
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filename), "UTF-8"));
+			String line;
+			int pos = 0;
+			m_gWeights = new double[m_featureSize+1];//to include the bias term
+			while((line=reader.readLine()) != null) {
+				m_gWeights[pos++] = Double.valueOf(line);
+			}
+			reader.close();
+		} catch(IOException e){
+			System.err.format("[Error]Fail to open file %s.\n", filename);
+		}
+	}
+	
+	public double calcDiffWsWg(){
+		int gid;
+		// Get a copy of super user's transformation matrix.
+		double[] As = Arrays.copyOfRange(m_A, m_userList.size()*m_dim*2, m_userList.size()*m_dim*2 + m_dimSup*2);
+		
+		// Set the bias term for ws.
+		m_sWeights[0] = As[0] * m_gWeights[0] + As[m_dimSup];
+		// Set the other terms for ws.
+		for(int n=0; n<m_featureSize; n++){
+			gid = m_featureGroupMap4SupUsr[1+n];
+			m_sWeights[n+1] = As[gid] * m_gWeights[1+n] + As[gid+ m_dimSup];
+		}
+		return Utils.EuclideanDistance(m_gWeights, m_sWeights);
+	}
+	
+	public double calcDiffWsWi(_AdaptStruct user){
+		_CoLinAdaptStruct ui = (_CoLinAdaptStruct) user;
+		int gid;
+					
+		//set bias term
+		m_pWeights[0] = ui.getScaling(0) * m_sWeights[0] + ui.getShifting(0);
+		//set the other features
+		for(int n=0; n<m_featureSize; n++) {
+			gid = m_featureGroupMap[1+n];
+			m_pWeights[1+n] = ui.getScaling(gid) * m_sWeights[1+n] + ui.getShifting(gid);
+		}
+		return Utils.EuclideanDistance(m_pWeights, m_sWeights);
 	}
 }
