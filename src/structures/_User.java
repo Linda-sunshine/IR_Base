f6fbb7f6f716b313bc7dc0b284faca11af8037cd
Linda-sunshine <gonglin1988@gmail.com>
2016-04-29 20:18:15 -0400

    modify _User

--- src/structures/_User.java
+++ src/structures/_User.java
@@ -112,25 +112,34 @@ public class _User {
 		return Utils.cosine(u.m_lowDimProfile, m_lowDimProfile);
 	}
 	
-//	public int predict(_Doc doc) {
-//		_SparseFeature[] fv = doc.getSparse();
-//		double maxScore = Utils.dotProduct(m_pWeight, fv, 0);
-//		if (m_classNo==2) {
-//			return maxScore>0?1:0;
-//		} else {//we will have k classes for multi-class classification
-//			double score;
-//			int pred = 0; 
-//			
-//			for(int i = 1; i < m_classNo; i++) {
-//				score = Utils.dotProduct(m_pWeight, fv, i * (m_featureSize + 1));
-//				if (score>maxScore) {
-//					maxScore = score;
-//					pred = i;
-//				}
-//			}
-//			return pred;
-//		}
-//	}
+	public int predict(_Doc doc) {
+		_SparseFeature[] fv = doc.getSparse();
+		// Original prediction.
+		if(m_pWeightB == null){
+		double maxScore = Utils.dotProduct(m_pWeight, fv, 0);
+			if (m_classNo==2) {
+				return maxScore>0?1:0;
+			} else {//we will have k classes for multi-class classification
+				double score;
+				int pred = 0; 
+			
+				for(int i = 1; i < m_classNo; i++) {
+					score = Utils.dotProduct(m_pWeight, fv, i * (m_featureSize + 1));
+					if (score>maxScore) {
+						maxScore = score;
+						pred = i;
+					}
+				}
+				return pred;
+			}
+			// Added by Lin. If we have weights for each class, currently we only have two classes.
+		} else{
+			double[] score = new double[2];
+			score[0] = dotProduct(m_pWeight, fv);
+			score[1] = dotProduct(m_pWeightB, fv);
+			return Utils.maxOfArrayIndex(score);
+		}
+	}
 	
 	// We need to consider the bias term.
 	public double dotProduct(double[] vct, _SparseFeature[] fvs){
@@ -139,16 +148,6 @@ public class _User {
 			sum += vct[fv.getIndex()+1]*fv.getValue();
 		}
 		return sum;
-	}
-	// if we have weights for each class.
-	public int predict(_Doc doc){
-		_SparseFeature[] fv = doc.getSparse();
-		double[] score = new double[2];
-		
-		score[0] = dotProduct(m_pWeight, fv);
-		score[1] = dotProduct(m_pWeightB, fv);
-		return Utils.maxOfArrayIndex(score);
-
 	}
 	
 	public void addOnePredResult(int predL, int trueL){
