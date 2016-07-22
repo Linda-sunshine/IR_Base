aedf8761fd247af6edbe8c21d02524462f243283
RenqinCai <jetcai1900@gmail.com>
2016-07-21 23:03:18 -0400

    add DCMCorrLDA_multi_E

--- src/topicmodels/DCMCorrLDA_multi.java
+++ src/topicmodels/DCMCorrLDA_multi.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-
 import structures._Corpus;
 import structures._Doc;
 import structures._ParentDoc;
