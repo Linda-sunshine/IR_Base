aae250ee10927eb3dc818cbe5828cdeca3192357
rc7ne <rc7ne>
2016-07-27 23:50:12 -0400

    fix a bug

--- src/topicmodels/correspondenceModels/DCMCorrLDA_multi.java
+++ src/topicmodels/correspondenceModels/DCMCorrLDA_multi.java
@@ -130,9 +130,11 @@ public class DCMCorrLDA_multi extends DCMCorrLDA{
 	
 	public DCMCorrLDA_multi(int number_of_iteration, double converge, double beta, 
 			_Corpus c, double lambda, int number_of_topics, double alpha_a, double alpha_c, double burnIn, 
-			int lag, int newtonIter, double newtonConverge) {
-		super(number_of_iteration, converge, beta, c, lambda, 
-				number_of_topics, alpha_a, alpha_c, burnIn, lag, newtonIter, newtonConverge);
+ int lag, double ksi,
+			double tau, int newtonIter, double newtonConverge) {
+		super(number_of_iteration, converge, beta, c, lambda, number_of_topics,
+				alpha_a, alpha_c, burnIn, ksi, tau, lag, newtonIter,
+				newtonConverge);
 		// TODO Auto-generated constructor stub
 		m_multithread = true;
 	}
