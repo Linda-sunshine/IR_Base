81b4e6aba54f58bc065816b6d1e390e4c70fce29
RenqinCai <jetcai1900@gmail.com>
2016-08-21 19:12:06 -0400

    add structure 1. _ChildDoc4DCMDMMCorrLDA

--- src/structures/_ChildDoc4DCMDMMCorrLDA.java
+++ src/structures/_ChildDoc4DCMDMMCorrLDA.java
@@ -6,7 +6,7 @@ public class _ChildDoc4DCMDMMCorrLDA extends _ChildDoc{
 	
 	public _ChildDoc4DCMDMMCorrLDA(int ID, String name, String title, String source, int ylabel){
 		super(ID, name, title, source, ylabel);
-		
+		m_topic = -1;
 	}
 	
 	public void setTopics4Gibbs(int k, double alpha){
@@ -17,6 +17,5 @@ public class _ChildDoc4DCMDMMCorrLDA extends _ChildDoc{
 		m_topic = tid;
 		m_sstat[tid] ++;
 	}
-	
 	
 }
