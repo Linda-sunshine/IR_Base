a5fd8cb6fc230cb0c8be607b4fcc53ae4739d5e9
Lu Lin <lulin199209@gmail.com>
2018-03-28 21:28:15 -0400

    add an topic embedding model for analyzing users' topic proportion  versus items' topic proportion

---
+++ src/Analyzer/ReviewAnalyzer.java
@@ -0,0 +1,7 @@
+package Analyzer;
+
+/**
+ * Created by lulin on 3/28/18.
+ */
+public class ReviewAnalyzer {
+}
---
+++ src/structures/_Doc4ETBIR.java
@@ -0,0 +1,7 @@
+package structures;
+
+/**
+ * Created by lulin on 3/28/18.
+ */
+public class _Doc4ETBIR {
+}
