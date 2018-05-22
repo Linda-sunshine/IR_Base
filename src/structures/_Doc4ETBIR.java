3f6f6579656c6d95229f54f995b5b793dfcd4677
Louise-LuLin <lulin199209@gmail.com>
2018-05-22 14:24:39 -0400

    _Doc4ETBIR extends from _Review

--- src/structures/_Doc4ETBIR.java
+++ src/structures/_Doc4ETBIR.java
@@ -6,7 +6,7 @@ import java.util.Random;
 /**
  * Created by lulin on 3/28/18.
  */
-public class _Doc4ETBIR extends _Doc{
+public class _Doc4ETBIR extends _Review{
 
     public double[] m_mu; // mean vector \mu in variational inference p(\theta|\mu,\Sigma)
     public double[] m_Sigma; // diagonal covariance matrix \Sigma in variational inference p(\theta|\mu,\Sigma)
@@ -15,7 +15,8 @@ public class _Doc4ETBIR extends _Doc{
     public double[] m_phiStat;// temporally store \sum_w c(w)p(z|w)
     
     public _Doc4ETBIR(int ID, String name, String prodID, String title, String source, int ylabel, long timeStamp){
-        super(ID,  name,  prodID,  title,  source,  ylabel,  timeStamp);
+//        super(ID,  name,  prodID,  title,  source,  ylabel,  timeStamp);
+        super(ID, source, ylabel, title,  prodID, "", timeStamp);
     }
     
     //create necessary structure for variational inference    
@@ -29,7 +30,7 @@ public class _Doc4ETBIR extends _Doc{
         m_phiStat = new double[k];
 
         for(int i = 0; i < k; i++){
-            m_mu[i] = mu + Math.random() * 0.5 * sigma;
+            m_mu[i] = mu + Math.random() * 0.5 * mu;
             m_Sigma[i] = sigma + Math.random() * 0.5 * sigma;
         }
   	}
