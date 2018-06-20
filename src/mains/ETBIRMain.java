d3a5b598eee6445ec1662186129d6e0967146fdf
Louise-LuLin <lulin199209@gmail.com>
2018-06-19 23:10:12 -0400

    pipeline for cross validation

--- src/mains/ETBIRMain.java
+++ src/mains/ETBIRMain.java
@@ -8,6 +8,8 @@ import Analyzer.MultiThreadedReviewAnalyzer;
 import Analyzer.MultiThreadedUserAnalyzer;
 import Analyzer.ReviewAnalyzer;
 import structures._Corpus;
+import structures._Doc;
+import structures._Review;
 import topicmodels.LDA.LDA_Gibbs;
 import topicmodels.LDA.LDA_Variational;
 import Analyzer.BipartiteAnalyzer;
@@ -16,6 +18,7 @@ import topicmodels.multithreads.LDA.LDA_Variational_multithread;
 import topicmodels.multithreads.embeddingModel.ETBIR_multithread;
 import topicmodels.multithreads.pLSA.pLSA_multithread;
 import topicmodels.pLSA.pLSA;
+import utils.Utils;
 
 
 /**
@@ -32,13 +35,25 @@ public class ETBIRMain {
         int numberOfCores = Runtime.getRuntime().availableProcessors();
         String tokenModel = "./data/Model/en-token.bin";
 
+        /*****parameters for topic model*****/
+        int number_of_topics = 20;
+        int varMaxIter = 30;
+        double varConverge = 1e-4;
+        int emMaxIter = 50;
+        double emConverge = 1e-12;
+
+        double alpha = 1 + 1e-2, beta = 1 + 1e-3, lambda = 1 + 1e-3;//these two parameters must be larger than 1!!!
+        double sigma = 1.01, rho = 1.01;
+
+        int topK = 50;
+        int crossV = 1;
+        boolean setRandomFold = true;
+
+        /*****data setting*****/
         String trainset = "byUser_4k_review";
         String source = "yelp";
         String dataset = "./myData/" + source + "/" + trainset + "/";
 
-        /**
-         * model training
-         */
         String[] fvFiles = new String[4];
         fvFiles[0] = "./data/Features/fv_2gram_IG_yelp_byUser_30_50_25.txt";
         fvFiles[1] = "./data/Features/fv_2gram_IG_amazon_movie_byUser_40_50_12.txt";
@@ -54,38 +69,29 @@ public class ETBIRMain {
         }
 
         String reviewFolder = dataset + "data/"; //2foldsCV/folder0/train/, data/
-        String outputFolder = dataset + "output/feature_infer_" + fvFile_point + "/";
-        String suffix = ".json";
-        String topicmodel = "ETBIR"; // pLSA, LDA_Gibbs, LDA_Variational, ETBIR
-
         MultiThreadedReviewAnalyzer analyzer = new MultiThreadedReviewAnalyzer(tokenModel, classNumber, fvFiles[fvFile_point],
                 Ngram, lengthThreshold, numberOfCores, true, source);
+        _Corpus corpus = new _Corpus();
+        if(crossV>1 && setRandomFold==false){
+            reviewFolder = dataset + crossV + "foldsCV/";
+        }else{
 //        analyzer.setReleaseContent(false);//Remember to set it as false when generating crossfolders!!!
-        analyzer.loadUserDir(reviewFolder);
-        _Corpus corpus = analyzer.getCorpus();
+            analyzer.loadUserDir(reviewFolder);
+            corpus = analyzer.getCorpus();
+        }
+
+        String outputFolder = dataset + "output/" + crossV + "foldsCV" + "/";
+        String suffix = ".json";
 
 //        corpus.save2File(dataset + "yelp_40_50_12.dat");//for CTM
 
-        int number_of_topics = 20;
-
-        int varMaxIter = 30;
-        double varConverge = 1e-4;
-
-        int emMaxIter = 50;
-        double emConverge = 1e-12;
-        double emConverge4ETBIR = 1e-8;
+        BipartiteAnalyzer cv = new BipartiteAnalyzer(corpus); // split corpus into folds
+        cv.analyzeCorpus();
+        cv.splitCorpus(crossV,dataset + crossV + "foldsCV/");
 
 
-        double alpha = topicmodel.equals("ETBIR")?1+1e-2:1+1e-2, beta = 1 + 1e-3, lambda = 1 + 1e-3;//these two parameters must be larger than 1!!!
-        double sigma = 1.01, rho = 1.01;
-
-        int topK = 50;
-        int crossV = 1;
-        boolean setRandomFold = true;
-
-        // LDA
-        /*****parameters for the two-topic topic model*****/
-
+        /*****model loading*****/
+        String topicmodel = "ETBIR"; // pLSA, LDA_Gibbs, LDA_Variational, ETBIR
         pLSA tModel = null;
         if (topicmodel.equals("pLSA")) {
             tModel = new pLSA_multithread(emMaxIter, emConverge, beta, corpus,
@@ -97,32 +103,60 @@ public class ETBIRMain {
             tModel = new LDA_Variational_multithread(emMaxIter, emConverge, beta, corpus,
                     lambda, number_of_topics, alpha, varMaxIter, varConverge); //set this negative!! or likelihood will not change
         } else if (topicmodel.equals("ETBIR")){
-            tModel = new ETBIR_multithread(emMaxIter, emConverge4ETBIR, beta, corpus, lambda,
+            tModel = new ETBIR_multithread(emMaxIter, emConverge, beta, corpus, lambda,
                     number_of_topics, alpha, varMaxIter, varConverge, sigma, rho);
         }else {
             System.out.println("The selected topic model has not developed yet!");
             return;
         }
 
-//        BipartiteAnalyzer cv = new BipartiteAnalyzer(corpus);
-//        cv.analyzeCorpus();
-//        cv.splitCorpus(crossV,dataset + crossV + "foldsCV/");
-
         tModel.setDisplayLap(1);
         new File(outputFolder).mkdirs();
         tModel.setInforWriter(outputFolder + topicmodel + "_info.txt");
-        if (crossV<=1) {
+        if (crossV<=1) {//just train
             tModel.EMonCorpus();
             tModel.printTopWords(topK, outputFolder + topicmodel + "_topWords.txt");
             tModel.printParameterAggregation(topK, outputFolder, topicmodel);
             tModel.closeWriter();
-        }else{
+        } else if(setRandomFold == true){//cross validation with random folds
             tModel.setRandomFold(setRandomFold);
             double trainProportion = ((double)crossV - 1)/(double)crossV;
             double testProportion = 1-trainProportion;
             tModel.setPerplexityProportion(testProportion);
             tModel.crossValidation(crossV);
             tModel.printTopWords(topK, outputFolder + topicmodel + "_topWords.txt");
+        } else{//cross validation with fixed folds
+            double[] perf = new double[crossV];
+            for(int k = 0; k <crossV; k++){
+                analyzer.reset();
+                //load test set
+                String testFolder = reviewFolder + k + "/";
+                analyzer.loadUserDir(testFolder);
+                for(_Doc d : analyzer.getCorpus().getCollection()){
+                    ((_Review)d).setType(_Review.rType.TEST);
+                }
+                //load train set
+                for(int i = 0; i < crossV; i++){
+                    if(i!=k){
+                        String trainFolder = reviewFolder + i + "/";
+                        analyzer.loadUserDir(trainFolder);
+                    }
+                }
+
+                tModel.setCorpus(analyzer.getCorpus());
+                perf[k] = tModel.oneFoldValidation();
+
+                String resultFolder = outputFolder + k + "/";
+                new File(resultFolder).mkdirs();
+                tModel.printParameterAggregation(topK, resultFolder, topicmodel);
+            }
+
+            //output the performance statistics
+            double mean = Utils.sumOfArray(perf)/crossV, var = 0;
+            for(int i=0; i<perf.length; i++)
+                var += (perf[i]-mean) * (perf[i]-mean);
+            var = Math.sqrt(var/crossV);
+            System.out.format("Perplexity %.3f+/-%.3f\n", mean, var);
         }
 
     }
