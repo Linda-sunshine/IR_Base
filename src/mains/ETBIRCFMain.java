9bd1a424213310fd0320f2523818460af331c5d0
Louise-LuLin <lulin199209@gmail.com>
2018-08-04 21:57:31 -0400

    CF pipeline: include 8 strategies and 3 models

--- src/mains/ETBIRCFMain.java
+++ src/mains/ETBIRCFMain.java
@@ -1,7 +1,9 @@
 package mains;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import opennlp.tools.util.InvalidFormatException;
@@ -19,8 +21,7 @@ public class ETBIRCFMain {
         int Ngram = 2; // The default value is unigram.
         int lengthThreshold = 5; // Document length threshold
         double trainRatio = 0, adaptRatio = 1;
-        int[] ks = new int[]{2, 4, 6, 8, 10, 12, 14}; // top_k neighbors
-        int crossV = 5;
+        int crossV = 2;
         int numberOfCores = Runtime.getRuntime().availableProcessors();
         boolean enforceAdapt = true;
         String tokenModel = "./data/Model/en-token.bin"; // Token model.
@@ -50,109 +51,190 @@ public class ETBIRCFMain {
             fvFile_point = 3;
         }
 
-        double[][] ndcg = new double[ks.length][crossV];
-        double[][] map = new double[ks.length][crossV];
-        for(int i = 0; i < crossV; i++) {
-            /***Loading data.***/
-            MultiThreadedLMAnalyzer analyzer = new MultiThreadedLMAnalyzer(tokenModel, classNumber, fvFiles[fvFile_point],
-                    lmFvFile, Ngram, lengthThreshold, numberOfCores, true);
-            analyzer.config(trainRatio, adaptRatio, enforceAdapt);
+        /*****experiment setting*****/
+        int[] neighborK = new int[]{2, 4, 6}; // top_k neighbors
+        int[] threshold = new int[]{10, 20, 30}; // popularity of item or time
+//        int[] topicNums = new int[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100};// number of topics
+        int[] topicNums = new int[]{20, 40};// number of topics
+        String[] models = new String[]{"ETBIR", "LDA_Variational", "CTM"};
+        /***meaning of different modes:
+         columnPhi: compare phi of document of the same item across different users (rowPhi: same user across different items)
+         columnPost: compare posterior parameter (\gamma or softmax(\mu)) of the same item across different users (rowPost: same user across different items)
+         columnProduct: compare the inner product of user's P and item's \eta of the same item across different users (rowProduct: same user across different items)
+         userEmbded: P for ETBIR, average over documents across users for LDA (\gamma) and CTM (softmax(\mu))
+         itemEmbed: \eta for ETBIR, average over documents across items for LDA (\gamma) and CTM (softmax(\mu)) */
+        String[] modes = new String[]{"rowPhi", "columnPhi", "rowPost", "columnPost", "userEmbed", "itemEmbed", "rowProduct", "columnProduct"};
 
-            String trainFolder, testFolder = String.format("%s/%d/", inputFolder, i);
-            //load train set
-            for(int j = 0; j < crossV; j++){
-                if(j != i){
-                    trainFolder = String.format("%s/%d/", inputFolder, j);
-                    analyzer.loadUserDir(trainFolder);
-                }
-            }
-            //need to explicitly allocate train-test for each user
-            for(_User u:analyzer.getUsers()){
-                u.constructTrainTestReviews();
-            }
-            //load test set
-            analyzer.loadTestUserDir(testFolder);
-            analyzer.setFeatureValues("TFIDF-sublinear", 0);
+        double[] ndcg, map;
+        int dim;
+        String mode, model;
+        String suffix1 = "txt", suffix2 = "classifer";
+        String neighborSelection = "all"; // "all"
 
-            /***Collaborative filtering starts here.***/
-            boolean equalWeight = false;
-            String saveAdjFolder = String.format("%s/%d/", outputFolder, i);
-            String dir, cfFile, model;
-            String[] models = new String[]{"ETBIR","LDA","CTM"};
-            String mode = "userP"; //row, column, userP, itemEta, productUserItem
-            String suffix1 = "txt", suffix2 = "classifer";
-            String neighborSelection = "all"; // "all"
+        MultiThreadedLMAnalyzer analyzer;
+        for(int th : threshold) {
+            for(int nk : neighborK) {
+                for (int m = 0; m < modes.length; m++) {
+                    mode = modes[m];
+                    for (int d = 0; d < models.length; d++) {
+                        model = models[d];
+                        System.out.format("===== %d threshould, %d neighbors, %s mode, %s model =====\n", th, nk, mode, model);
 
-            /***
-             * In order to perform cf, we need to follow the following steps:
-             * Step 1: construct ranking neighbors using the same CollaborativeFiltering.java
-             * Step 2: perform collaborative filtering */
-
-            // Step 1: construct ranking neighbors using the same CollaborativeFiltering.java
-            CollaborativeFiltering cfInit = new CollaborativeFiltering(analyzer.getUsers(), analyzer.getFeatureSize() + 1);
-            int dim = 20;
-            int[] threshold = new int[]{3, 5, 7, 10};
-
-            for (int th : threshold) {
-                dir = String.format("%s/%s_cf_%s_%d_", saveAdjFolder, dataset, neighborSelection, th);
-                // construct ranking neighbors
-                cfInit.constructRankingNeighbors(neighborSelection, th);
-                cfInit.saveUserItemPairs(dir);
-            }
-
-            // Step 2: perform collaborative filtering
-            for (int th : threshold) { // threshold: time or popularity
-                for (int k = 0; k < ks.length; k++) {
-                    // load the saved neighbor file
-                    cfFile = String.format("%s/%s_cf_%s_%d_test.csv", saveAdjFolder, dataset, neighborSelection, th);
-                    ArrayList<_User> cfUsers = cfInit.getUsers();
-                    cfInit.loadRankingCandidates(cfFile);
-
-                    int validUser = cfInit.getValidUserSize();
-
-                    for(int m = 0; m < models.length; m++) {
-                        model = models[m];
-                        System.out.format("\n-----------------run %s %d neighbors-------------------------\n", model, ks[k]);
-                        CollaborativeFilteringWithETBIR cf = new CollaborativeFilteringWithETBIR(cfUsers, analyzer.getFeatureSize() + 1, ks[k], dim);
-                        cf.setValidUserSize(validUser);
-                        cf.setEqualWeightFlag(equalWeight);
-                        cf.setMode(mode);
-
-                        if(model.equals("ETBIR")) {
-                            String userWeight = String.format("%s/%d/%s_postNu.txt", outputFolder, i, model);//user embedding
-                            String itemWeight = String.format("%s/%d/%s_postEta.txt", outputFolder, i, model);//item embedding
-                            cf.loadWeights(userWeight, model, suffix1, suffix2);
-                            cf.loadItemWeights(itemWeight);
-                        } else if(model.equals("LDA")){
-                            String userWeight = String.format("%s/%d/%s_postByUser.txt", outputFolder, i, model);
-                            String itemWeight = String.format("%s/%d/%s_postByItem.txt", outputFolder, i, model);
+                        //name: threshold-nk-mode-model.txt
+                        //content: line1 is NDCG-mean, line2 is NDCG-var, line3 is MAP-mean, line4 is MAP-var
+                        String resultFile = String.format("%s/CF/%d-%d-%s-%s.txt", outputFolder, th, nk, mode, model);
+                        File file = new File(resultFile);
+                        try {
+                            file.getParentFile().mkdirs();
+                            file.createNewFile();
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
+                        try {
+                            PrintWriter resultWriter = new PrintWriter(file);
+                            for(int num : topicNums){
+                                resultWriter.format("%d,",num);
+                            }
+                            resultWriter.println();
 
-                        cf.calculateAllNDCGMAP();
-                        cf.calculateAvgNDCGMAP();
+                            double[][] results = new double[4][topicNums.length];
+                            for (int n = 0; n < topicNums.length; n++) {
+                                dim = topicNums[n];
+                                if ((mode.equals("columnProduct") || mode.equals("rowProduct")) && (!model.equals("ETBIR"))) {
+                                    System.err.format("[Warning]%s mode is only for ETBIR, not for %s.\n", mode, model);
+                                    continue;
+                                }
 
-                        System.out.format("\n[Info]NDCG: %.4f, MAP: %.4f\n", cf.getAvgNDCG(), cf.getAvgMAP());
-                        ndcg[k][i] = cf.getAvgNDCG();
-                        map[k][i] = cf.getAvgMAP();
+                                ndcg = new double[crossV];
+                                map = new double[crossV];
+                                for (int i = 0; i < crossV; i++) {
+                                    /***Loading data.***/
+                                    analyzer = new MultiThreadedLMAnalyzer(tokenModel, classNumber, fvFiles[fvFile_point],
+                                            lmFvFile, Ngram, lengthThreshold, numberOfCores, true);
+                                    analyzer.config(trainRatio, adaptRatio, enforceAdapt);
+
+                                    String trainFolder, testFolder = String.format("%s/%d/", inputFolder, i);
+                                    //load train set
+                                    for (int j = 0; j < crossV; j++) {
+                                        if (j != i) {
+                                            trainFolder = String.format("%s/%d/", inputFolder, j);
+                                            analyzer.loadUserDir(trainFolder);
+                                        }
+                                    }
+                                    //need to explicitly allocate train-test for each user
+                                    for (_User u : analyzer.getUsers()) {
+                                        u.constructTrainTestReviews();
+                                    }
+                                    //load test set
+                                    analyzer.loadTestUserDir(testFolder);
+                                    analyzer.setFeatureValues("TFIDF-sublinear", 0);
+
+                                    /***Collaborative filtering starts here.***/
+                                    boolean equalWeight = false;
+                                    String saveAdjFolder = String.format("%s/%d/", outputFolder, i);
+                                    String dir, cfFile;
+
+
+                                    /***
+                                     * In order to perform cf, we need to follow the following steps:
+                                     * Step 1: construct ranking neighbors using the same CollaborativeFiltering.java
+                                     * Step 2: perform collaborative filtering */
+
+                                    // Step 1: construct ranking neighbors using the same CollaborativeFiltering.java
+                                    CollaborativeFiltering cfInit = new CollaborativeFiltering(analyzer.getUsers(), analyzer.getFeatureSize() + 1);
+                                    // construct ranking neighbors
+                                    dir = String.format("%s/%s_cf_%s_%d_", saveAdjFolder, dataset, neighborSelection, th);
+                                    cfInit.constructRankingNeighbors(neighborSelection, th);
+                                    cfInit.saveUserItemPairs(dir);
+
+                                    // Step 2: perform collaborative filtering
+                                    // load candidate items from the saved neighbor file
+                                    cfFile = String.format("%s/%s_cf_%s_%d_test.csv", saveAdjFolder, dataset, neighborSelection, th);
+                                    ArrayList<_User> cfUsers = cfInit.getUsers();
+                                    cfInit.loadRankingCandidates(cfFile);
+
+                                    int validUser = cfInit.getValidUserSize();
+                                    System.out.format("\n-----------------run %s model for %s mode-------------------------\n", model, mode);
+                                    CollaborativeFilteringWithETBIR cf = new CollaborativeFilteringWithETBIR(cfUsers, analyzer.getFeatureSize() + 1, nk, dim);
+                                    cf.setValidUserSize(validUser);
+                                    cf.setEqualWeightFlag(equalWeight);
+                                    cf.setMode(mode);
+                                    cf.setModel(model);
+
+                                    String userWeight = "", itemWeight = "", docWeight = "";
+                                    if (mode.equals("rowPhi") || mode.equals("columnPhi")) {
+                                        userWeight = String.format("%s/%d/%s_phiByUser_%d.txt", outputFolder, i, model, dim);//user embedding
+                                        itemWeight = String.format("%s/%d/%s_phiByItem_%d.txt", outputFolder, i, model, dim);//item embedding
+                                        docWeight = String.format("%s/%d/%s_phi_%d.txt", outputFolder, i, model, dim);//doc embedding
+                                    } else if (mode.equals("rowPost") || mode.equals("columnPost")) {
+                                        userWeight = String.format("%s/%d/%s_postByUser_%d.txt", outputFolder, i, model, dim);//user embedding
+                                        itemWeight = String.format("%s/%d/%s_postByItem_%d.txt", outputFolder, i, model, dim);//item embedding
+                                        if (model.equals("LDA_Variational"))
+                                            docWeight = String.format("%s/%d/%s_postGamma_%d.txt", outputFolder, i, model, dim);//doc embedding
+                                        else
+                                            docWeight = String.format("%s/%d/%s_postSoftmax_%d.txt", outputFolder, i, model, dim);//doc embedding
+                                    } else if (mode.equals("userEmbed") || mode.equals("itemEmbed")) {
+                                        if (model.equals("ETBIR")) {
+                                            userWeight = String.format("%s/%d/%s_postNu_%d.txt", outputFolder, i, model, dim);//user embedding
+                                            itemWeight = String.format("%s/%d/%s_postEta_%d.txt", outputFolder, i, model, dim);//item embedding
+                                            docWeight = String.format("%s/%d/%s_phi_%d.txt", outputFolder, i, model, dim);//doc embedding, here won't be used
+                                        } else {
+                                            userWeight = String.format("%s/%d/%s_postByUser_%d.txt", outputFolder, i, model, dim);//user embedding
+                                            itemWeight = String.format("%s/%d/%s_postByItem_%d.txt", outputFolder, i, model, dim);//item embedding
+                                            docWeight = String.format("%s/%d/%s_phi_%d.txt", outputFolder, i, model, dim);//doc embedding, here won't be used
+                                        }
+                                    } else if (mode.equals("rowProduct") || mode.equals("columnProduct")) {
+                                        userWeight = String.format("%s/%d/%s_postNu_%d.txt", outputFolder, i, model, dim);//user embedding
+                                        itemWeight = String.format("%s/%d/%s_postEta_%d.txt", outputFolder, i, model, dim);//item embedding
+                                        docWeight = String.format("%s/%d/%s_phi_%d.txt", outputFolder, i, model, dim);//doc embedding, here won't be used
+                                    }
+
+                                    cf.loadWeights(userWeight, model, suffix1, suffix2);
+                                    cf.loadItemWeights(itemWeight, model);
+                                    cf.loadReviewTopicWeights(docWeight);
+
+                                    cf.calculateAllNDCGMAP();
+                                    cf.calculateAvgNDCGMAP();
+
+                                    System.out.format("\n[Info]NDCG: %.4f, MAP: %.4f\n", cf.getAvgNDCG(), cf.getAvgMAP());
+                                    ndcg[i] = cf.getAvgNDCG();
+                                    map[i] = cf.getAvgMAP();
+                                }
+
+                                //printout the performance statistics
+                                double mean1 = Utils.sumOfArray(ndcg) / crossV, var1 = 0;
+                                for (int i = 0; i < ndcg.length; i++)
+                                    var1 += (ndcg[i] - mean1) * (ndcg[i] - mean1);
+                                var1 = Math.sqrt(var1 / crossV);
+                                System.out.format("[Stat-NDCG]%d threshold, %d neighbors, %d topic number, %s mode, %s model: %.4f+/-%.4f\n",
+                                        th, nk, dim, mode, model, mean1, var1);
+
+                                double mean2 = Utils.sumOfArray(map) / crossV, var2 = 0;
+                                for (int i = 0; i < map.length; i++)
+                                    var2 += (map[i] - mean2) * (map[i] - mean2);
+                                var2 = Math.sqrt(var2 / crossV);
+                                System.out.format("[Stat-MAP]%d threshold, %d neighbors, %d topic number, %s mode, %s model: %.4f+/-%.4f\n",
+                                        th, nk, dim, mode, model, mean2, var2);
+
+                                results[0][n] = mean1;
+                                results[1][n] = var1;
+                                results[2][n] = mean2;
+                                results[3][n] = var2;
+                            }
+
+                            for(int i = 0; i < 4; i++) {
+                                for (int n = 0; n < topicNums.length; n++) {
+                                    resultWriter.format("%.4f,",results[i][n]);
+                                }
+                                resultWriter.println();
+                            }
+                            resultWriter.close();
+                        } catch(FileNotFoundException ex){
+                            System.err.format("[Error]Failed to open file %s\n", resultFile);
+                        }
                     }
                 }
             }
-        }
-
-        //output the performance statistics
-        for(int k = 0; k < ks.length; k++) {
-            double mean = Utils.sumOfArray(ndcg[k]) / crossV, var = 0;
-            for (int i = 0; i < ndcg[k].length; i++)
-                var += (ndcg[k][i] - mean) * (ndcg[k][i] - mean);
-            var = Math.sqrt(var / crossV);
-            System.out.format("[Stat]NDCG %.4f+/-%.4f\n", mean, var);
-
-            mean = Utils.sumOfArray(map[k]) / crossV;
-            var = 0;
-            for (int i = 0; i < map[k].length; i++)
-                var += (map[k][i] - mean) * (map[k][i] - mean);
-            var = Math.sqrt(var / crossV);
-            System.out.format("[Stat]MAP %.4f+/-%.4f\n", mean, var);
         }
     }
 }
