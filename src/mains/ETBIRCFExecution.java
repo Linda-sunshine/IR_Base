f9455585df3d410cda47e516203aded65a246799
Louise-LuLin <lulin199209@gmail.com>
2018-08-13 05:08:13 -0400

    CF: pipeline done

--- src/mains/ETBIRCFExecution.java
+++ src/mains/ETBIRCFExecution.java
@@ -1,56 +1,38 @@
 package mains;
 
+import Analyzer.MultiThreadedLMAnalyzer;
+import Application.CollaborativeFiltering;
+import Application.CollaborativeFilteringWithETBIR;
+import structures.TopicModelParameter;
+import structures._User;
+import utils.Utils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
 import java.util.ArrayList;
 
-import opennlp.tools.util.InvalidFormatException;
-import structures._User;
-import Analyzer.MultiThreadedLMAnalyzer;
-import Analyzer.MultiThreadedReviewAnalyzer;
-import Application.CollaborativeFiltering;
-import Application.CollaborativeFilteringWithETBIR;
-import utils.Utils;
+public class ETBIRCFExecution {
+    public static void main(String[] args) throws IOException, ParseException {
+        TopicModelParameter param = new TopicModelParameter(args);
 
-public class ETBIRCFMain {
-    public static void main(String[] args) throws InvalidFormatException, FileNotFoundException, IOException{
-
-        int classNumber = 5;
-        int Ngram = 2; // The default value is unigram.
-        int lengthThreshold = 5; // Document length threshold
+        int classNumber = 6; //Define the number of classes in this Naive Bayes.
+        int Ngram = 2; //The default value is unigram.
+        int lengthThreshold = 5; //Document length threshold
         double trainRatio = 0, adaptRatio = 1;
-        int crossV = 5;
-        int numberOfCores = Runtime.getRuntime().availableProcessors();
         boolean enforceAdapt = true;
-        String tokenModel = "./data/Model/en-token.bin"; // Token model.
-        String fs = "DF";//"IG_CHI"
-        int lmTopK = 1000; // topK for language model.
+        boolean setRandomFold = false;
+        int numberOfCores = Runtime.getRuntime().availableProcessors();
         String lmFvFile = null;
-//        String lmFvFile = String.format("./data/CoLinAdapt/%s/fv_lm_%s_%d.txt", dataset, fs, lmTopK);
 
-        /*****data setting*****/
-        String locate = "./myData";
-        String scaleset = "byUser_70k_review";
-        String dataset = "yelp";
-        String folder = String.format("%s/%s/%s",locate, dataset, scaleset);
-        String inputFolder = String.format("%s/%dfoldsCV", folder, crossV);
-        String outputFolder = String.format("%s/output/%dfoldsCV", folder, crossV);
+        String tokenModel = "./data/Model/en-token.bin";
+        String folder = String.format("%s/%s/%s", param.m_prefix, param.m_source, param.m_set);
+        String fvFile = String.format("%s/%s/%s_features.txt", param.m_prefix, param.m_source, param.m_source);
 
-        String[] fvFiles = new String[4];
-        fvFiles[0] = "./data/Features/fv_2gram_IG_yelp_byUser_30_50_25.txt";
-        fvFiles[1] = "./data/Features/fv_2gram_IG_amazon_movie_byUser_40_50_12.txt";
-        fvFiles[2] = "./data/Features/fv_2gram_IG_amazon_electronic_byUser_20_20_5.txt";
-        fvFiles[3] = "./data/Features/fv_2gram_IG_amazon_book_byUser_40_50_12.txt";
-        int fvFile_point = 0;
-        if(dataset.equals("amazon_movie")){
-            fvFile_point = 1;
-        }else if(dataset.equals("amazon_electronic")){
-            fvFile_point = 2;
-        }else if(dataset.equals("amazon_book")){
-            fvFile_point = 3;
-        }
+        String inputFolder = String.format("%s/%dfoldsCV%s", folder,  param.m_crossV, param.m_flag_coldstart?"Coldstart":"");
+        String outputFolder = String.format("%s/output/%dfoldsCV%s", folder,  param.m_crossV, param.m_flag_coldstart?"Coldstart":"");
 
         /*****experiment setting*****/
         int[] neighborK = new int[]{2,4,6}; // top_k neighbors
@@ -87,7 +69,7 @@ public class ETBIRCFMain {
                                 continue;
                             }
 
-                            String resultFile = String.format("%s/CF/%s_%d_%d_%s_%s_%d.txt", outputFolder, dataset, th, nk, model, mode, dim);
+                            String resultFile = String.format("%s/CF/CF_%s_%d_%d_%s_%s_%d.txt", outputFolder, param.m_source, th, nk, model, mode, dim);
                             File file = new File(resultFile);
                             try {
                                 file.getParentFile().mkdirs();
@@ -98,17 +80,17 @@ public class ETBIRCFMain {
                             try {
                                 PrintWriter resultWriter = new PrintWriter(file);
 
-                                ndcg = new double[crossV];
-                                map = new double[crossV];
-                                for (int i = 0; i < crossV; i++) {
+                                ndcg = new double[param.m_crossV];
+                                map = new double[param.m_crossV];
+                                for (int i = 0; i < param.m_crossV; i++) {
                                     /***Loading data.***/
-                                    analyzer = new MultiThreadedLMAnalyzer(tokenModel, classNumber, fvFiles[fvFile_point],
+                                    analyzer = new MultiThreadedLMAnalyzer(tokenModel, classNumber, fvFile,
                                             lmFvFile, Ngram, lengthThreshold, numberOfCores, true);
                                     analyzer.config(trainRatio, adaptRatio, enforceAdapt);
 
                                     String trainFolder, testFolder = String.format("%s/%d/", inputFolder, i);
                                     //load train set
-                                    for (int j = 0; j < crossV; j++) {
+                                    for (int j = 0; j < param.m_crossV; j++) {
                                         if (j != i) {
                                             trainFolder = String.format("%s/%d/", inputFolder, j);
                                             analyzer.loadUserDir(trainFolder);
@@ -136,16 +118,17 @@ public class ETBIRCFMain {
                                     // Step 1: construct ranking neighbors using the same CollaborativeFiltering.java
                                     CollaborativeFiltering cfInit = new CollaborativeFiltering(analyzer.getUsers(), analyzer.getFeatureSize() + 1);
                                     // construct ranking neighbors
-                                    dir = String.format("%s/%s_cf_%s_%d_", saveAdjFolder, dataset, neighborSelection, th);
+                                    dir = String.format("%s/%s_cf_%s_%d_", saveAdjFolder, param.m_source, neighborSelection, th);
                                     cfInit.constructRankingNeighbors(neighborSelection, th);
-                                    cfInit.saveUserItemPairs(dir);
 
                                     // Step 2: perform collaborative filtering
                                     // load candidate items from the saved neighbor file
-                                    cfFile = String.format("%s/%s_cf_%s_%d_test.csv", saveAdjFolder, dataset, neighborSelection, th);
-                                    ArrayList<_User> cfUsers = cfInit.getUsers();
-                                    cfInit.loadRankingCandidates(cfFile);
+//                                    cfInit.saveUserItemPairs(dir);
+//                                    cfFile = String.format("%s/%s_cf_%s_%d_test.csv", saveAdjFolder, param.m_source, neighborSelection, th);
+//                                    cfInit.loadRankingCandidates(cfFile);
+                                    cfInit.constructRankingCandidates();
 
+                                    ArrayList<_User> cfUsers = cfInit.getUsers();
                                     int validUser = cfInit.getValidUserSize();
                                     System.out.format("\n-----------------run %s model for %s mode-------------------------\n", model, mode);
                                     CollaborativeFilteringWithETBIR cf = new CollaborativeFilteringWithETBIR(cfUsers, analyzer.getFeatureSize() + 1, nk, dim);
@@ -195,17 +178,17 @@ public class ETBIRCFMain {
                                 }
 
                                 //printout the performance statistics
-                                double mean1 = Utils.sumOfArray(ndcg) / crossV, var1 = 0;
+                                double mean1 = Utils.sumOfArray(ndcg) / param.m_crossV, var1 = 0;
                                 for (int i = 0; i < ndcg.length; i++)
                                     var1 += (ndcg[i] - mean1) * (ndcg[i] - mean1);
-                                var1 = Math.sqrt(var1 / crossV);
+                                var1 = Math.sqrt(var1 / param.m_crossV);
                                 System.out.format("[Stat-NDCG]%d threshold, %d neighbors, %d topic number, %s mode, %s model: %.4f+/-%.4f\n",
                                         th, nk, dim, mode, model, mean1, var1);
 
-                                double mean2 = Utils.sumOfArray(map) / crossV, var2 = 0;
+                                double mean2 = Utils.sumOfArray(map) / param.m_crossV, var2 = 0;
                                 for (int i = 0; i < map.length; i++)
                                     var2 += (map[i] - mean2) * (map[i] - mean2);
-                                var2 = Math.sqrt(var2 / crossV);
+                                var2 = Math.sqrt(var2 / param.m_crossV);
                                 System.out.format("[Stat-MAP]%d threshold, %d neighbors, %d topic number, %s mode, %s model: %.4f+/-%.4f\n",
                                         th, nk, dim, mode, model, mean2, var2);
 
