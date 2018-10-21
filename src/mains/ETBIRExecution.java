3a4eae2502550f51531489271c1447dffeeddcd6
Lu Lin <lulin199209@gmail.com>
2018-10-21 13:38:56 -0400

    minor

--- src/mains/ETBIRExecution.java
+++ src/mains/ETBIRExecution.java
@@ -4,11 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringJoiner;
 
-import Analyzer.BipartiteAnalyzer;
 import structures.TopicModelParameter;
 import structures._Corpus;
 import structures._Doc;
