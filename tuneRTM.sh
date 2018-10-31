8be5c68bf7da5ab0df6079870dd26f9403612767
Lu Lin <lulin199209@gmail.com>
2018-10-31 17:51:05 -0400

    add flag for coldstart in bash

--- tuneRTM.sh
+++ tuneRTM.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-read -p "Enter source, model, mode:" source model mode
+read -p "Enter source, model, mode, coldstart: " source model mode cold
 echo
-nohup ./runRTM -prefix ~/lab/dataset -source $source -set byUser_20k_review -topicmodel $model -mode $mode > ./output_data/"$mode"_"$source"_"$model".output 2>&1 &
+nohup ./runRTM -prefix ~/lab/dataset -source $source -set byUser_20k_review -topicmodel $model -mode $mode -flagColdstart $cold > ./output_data/"$mode"_"$cold"_"$source"_"$model".output 2>&1 &
