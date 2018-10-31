72a38b35623eb3fa0977ed627c251bc308381bf9
Lu Lin <lulin199209@gmail.com>
2018-10-30 23:29:07 -0400

    minor for bash

--- tuneLDA.sh
+++ tuneLDA.sh
@@ -3,5 +3,5 @@ read -p "Enter source, crossV, model:" source crossV model
 echo
 for k in {5..50..5}
 do
-    nohup ./run -prefix ~/dataset -source $source -set byUser_20k_review -topicmodel $model -crossV $crossV -nuOfTopics $k  -emIter 50 > ./output/"$crossV"_"$source"_"$model"_"$k".output 2>&1 &
+    nohup ./run -prefix ~/lab/dataset -source $source -set byUser_20k_review -topicmodel $model -crossV $crossV -nuOfTopics $k  -emIter 50 > ./output/"$crossV"_"$source"_"$model"_"$k".output 2>&1 &
 done
