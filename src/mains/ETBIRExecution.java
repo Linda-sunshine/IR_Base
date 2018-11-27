771cfb859388a7400ad5ff15940deafab1034187
Louise-LuLin <lulin199209@gmail.com>
2018-11-27 11:58:40 -0500

    minor

--- src/mains/ETBIRExecution.java
+++ src/mains/ETBIRExecution.java
@@ -138,6 +138,8 @@ public class ETBIRExecution {
                 }
                 tModel.setCorpus(analyzer.getCorpus());
 
+                System.out.format("====================\n[Info]Fold No. %d: \n", k);
+                double[] results = tModel.oneFoldValidation();
                 for(int i = 0; i < result_dim; i++){
                     perf[k][i] = results[2*i];
                     like[k][i] = results[2*i+1];
